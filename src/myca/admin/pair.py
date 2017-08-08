1a3465aafe4b1a430b921b7d5721dc4480e4f2df
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-08-08 12:41:11 +0300

    revert cert

--- src/myca/admin/pair.py
+++ src/myca/admin/pair.py
@@ -1,4 +1,6 @@
-from flask import request
+from flask import request, flash, redirect
+from flask_admin import expose
+from flask_admin.helpers import get_redirect_target
 
 from myca import models
 from .base import ModelView
@@ -10,6 +12,7 @@ class PairView(ModelView):
     can_edit = False
 
     column_list = ['issued_at']
+    list_template = 'admin/pair_list.html'
     details_template = 'admin/pair_details.html'
 
     def is_visible(self):
@@ -28,3 +31,14 @@ class PairView(ModelView):
         if identity_id:
             query = query.filter_by(identity_id=identity_id)
         return query
+
+    @expose('/revert/', methods=['POST'])
+    def revert_view(self):
+        old_pair = self.get_one(request.values.get('id'))
+        new_pair = models.Pair(old_pair.cert, old_pair.key)
+        new_pair.identity_id = old_pair.identity_id
+        self.session.add(new_pair)
+        self.session.commit()
+        return_url = get_redirect_target() or self.get_url('identity.index_view')
+        flash('The certificate was successfully reverted', 'success')
+        return redirect(return_url)
